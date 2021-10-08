212acea2422a3746d043abeaf61b1ff00118122e
Paulo Sergio <pamorimmonico@gmail.com>
2021-10-08 11:03:45 -0300

    a

--- src/main/java/cdp/Pessoa.java
+++ src/main/java/cdp/Pessoa.java
@@ -8,11 +8,16 @@ import java.time.LocalDate;
 @MappedSuperclass
 public abstract class Pessoa {
     @Id
+    @Column(name = "nome", nullable = false, length = 45)
     private String nome;
-    private LocalDate data_nascimento;
-    private long cpf;
 
-    public Pessoa(String nome, LocalDate data_nascimento, long cpf) {
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate data_nascimento;
+
+    @Column(name = "cpf", nullable = false)
+    private int cpf;
+
+    public Pessoa(String nome, LocalDate data_nascimento, int cpf) {
         this.nome = nome;
         this.data_nascimento = data_nascimento;
         this.cpf = cpf;
@@ -20,15 +25,17 @@ public abstract class Pessoa {
 
     public Pessoa() { }
 
-    public String getNome() { return nome; }
+    public int getCpf() { return cpf; }
 
-    public void setNome(String nome) { this.nome = nome; }
+    public void setCpf(int cpf) { this.cpf = cpf; }
 
     public LocalDate getData_nascimento() { return data_nascimento; }
 
     public void setData_nascimento(LocalDate data_nascimento) { this.data_nascimento = data_nascimento; }
 
-    public long getCpf() { return cpf; }
+    public String getNome() { return nome; }
 
-    public void setCpf(long cpf) { this.cpf = cpf; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public int getIdade() { return LocalDate.now().getYear() - data_nascimento.getYear(); }
 }
