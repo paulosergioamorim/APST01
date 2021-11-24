5b12eb38feef5067d596394cab4dd58826f615d7
Paulo Sergio <pamorimmonico@gmail.com>
2021-11-23 21:31:17 -0300

    DAOs created

--- src/main/java/cgd/dao/DAO.java
+++ src/main/java/cgd/dao/DAO.java
@@ -3,7 +3,6 @@ package cgd.dao;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
