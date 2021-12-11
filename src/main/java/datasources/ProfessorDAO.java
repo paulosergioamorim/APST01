1015ea32b91c697a88a58f0e320eebbc547a96fa
Paulo Sergio <pamorimmonico@gmail.com>
2021-12-10 23:34:49 -0300

    a

--- src/main/java/datasources/ProfessorDAO.java
+++ src/main/java/datasources/ProfessorDAO.java
@@ -1,28 +1,33 @@
 package datasources;
 
+import models.Estado;
 import models.entitys.Professor;
+import models.entitys.Turma;
 import org.hibernate.cfg.Configuration;
 import org.jetbrains.annotations.NotNull;
+
+import javax.persistence.Query;
+import java.util.List;
+
+import static models.Estado.FECHADA;
 
 public class ProfessorDAO extends DAO<Professor, Long> {
     public ProfessorDAO(Configuration configuration, @NotNull Class<Professor> entity) {
         super(configuration, entity);
     }
 
+    @SuppressWarnings("unchecked")
     public boolean isActive(Professor professor) {
         this.open();
         try {
-            String sql =
-            """
+            String hql = """
                     from Turma
                     where responsavel = :professor
-                    and estado != 'Fechada'
-                    and dataInicio < current_date
-                    and dataFim > current_date
                     """;
-            return !session.createQuery(sql)
+            List<Turma> turmas = session.createQuery(hql)
                     .setParameter("professor", professor)
-                    .list().isEmpty();
+                    .getResultList();
+            return turmas.stream().anyMatch(turma -> turma.getEstado() != FECHADA);
         } catch (Exception e) {
             e.printStackTrace();
             return false;
